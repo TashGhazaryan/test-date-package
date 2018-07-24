947a4fd3df51b17b65f77f3c279451a861acbe04
artashghazaryan <artash.ghazaryan999@gmail.com>
2018-07-24 22:36:56 +0400

    removed unnecessary function

--- test/first.test.js
+++ test/first.test.js
@@ -1,8 +1,5 @@
 const expect = require("chai").expect;
 const getDate = require("../src/dateService").getDate;
-function hello() {
-  return "Hello";
-}
 
 describe("Server info @post-deploy", () => {
   it("tests if server/info endpoint returns timestamp", () => {
